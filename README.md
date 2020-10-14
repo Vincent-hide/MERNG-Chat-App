639f56114b5494371b590961360e2b9f50081cdb
Vincent-hide <hidehiro.okayama@gmail.com>
2020-10-14 17:07:29 -0400

    express replaced with apollo-server

---
+++ README.md
@@ -0,0 +1,7 @@
+# Chat Application with MERNG
+
+* fontend: React.js
+* backend: Express/Node.js
+* Database: MongoDB
+* API query: GraphQL
+
--- index.js
+++ index.js
@@ -1,39 +1,69 @@
-const { graphql, buildSchema } = require('graphql');
+const { ApolloServer, gql } = require('apollo-server');
+const crypto = require('crypto');
 
 const db = {
   users: [
-    { id: "1", email: "john@gmail.com", name: "John" },
-    { id: "2", email: "max@gmail.com", name: "Max" },
+    { id: "1", email: "john@gmail.com", name: "John", avatarUrl: "https://avatar.com" },
+    { id: "2", email: "max@gmail.com", name: "Max", avatarUrl: "https://avatar.com" },
+  ],
+    messages: [
+    { id: "1", userId: '1', body: 'Grape', createdAt: Date.now() },
+    { id: "2", userId: '2', body: 'Grapefruits', createdAt: Date.now() },
+    { id: "3", userId: '1', body: 'Blueberry', createdAt: Date.now() },
   ]
-}
+};
 
-const schema = buildSchema(`
+const typeDefs = gql`
   type Query {
     users: [User!]!
+    user(id: ID!): User
+    messages: [Message!]!
+  }
+
+  type Mutation {
+    addUser(email: String!, name: String): User
   }
 
   type User {
     id: ID!
     email: String!
     name: String
-    avatarUrl: String
+    avatarUrl: String,
+    messages: [Message!]!
   }
-`)
 
-const rootValue = {
-  users: () => db.users
+  type Message {
+    id: ID!
+    body: String!
+    createdAt: String
+  }
+
+`
+
+const resolvers = {
+  Query: {
+    users: () => db.users,
+    user: (root, args) => db.users.find(user => user.id === args.id),
+    messages: () => db.messages,    
+  },
+  Mutation: {
+    addUser: (root, { email, name }) => {
+      const user = {
+        id: crypto.randomBytes(10).toString('hex'),
+        email,
+        name
+      }
+
+      db.users.push(user);
+      return user;
+    }
+  },
+  User: {
+    messages: user => db.messages.filter(message => message.userId === user.id)
+  }
 }
 
-graphql(
-  schema,
-  `
-    {
-      users {
-        email
-      }
-    }
-  `,
-  rootValue
-)
-  .then(res => console.dir(res, { depth: null }))
-  .catch(console.error)
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen()
+  .then(({ url }) => console.log(url));
